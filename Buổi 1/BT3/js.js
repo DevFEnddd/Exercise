9cd8fea8e28025110b627da1ff82fb30fe8f98ec
DevFEnddd <88378029+DevFEnddd@users.noreply.github.com>
2021-08-03 17:32:39 +0700

    Update js.js

--- Buổi 1/BT3/js.js
+++ Buổi 1/BT3/js.js
@@ -1,4 +1,4 @@
 // Write a script that calculates the area of a square
-let squareEgde = prompt('Enter side length of the square');
+let squareEgde = Number(prompt('Enter side length of the square'));
 let squareArea = squareEgde * squareEgde;
 alert(`The square area is ${squareArea}`)
